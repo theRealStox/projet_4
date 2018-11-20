8eadae82a3669a41abb2efdd86c2b80915322559
theRealStox <33998824+theRealStox@users.noreply.github.com>
2018-11-20 23:03:26 +0100

    modification du contenu du dossier class

--- class/Autoload__stack.php
+++ class/Autoload__stack.php
@@ -1,7 +1,7 @@
 <?php
 function chargerClasse($classe)
 {
-  require $classe . '.php'; // On inclut la classe correspondante au paramètre passé.
+  require 'model/Class__' . $classe . '.php'; // On inclut la classe correspondante au paramètre passé.
 }
 
 spl_autoload_register('chargerClasse'); // On enregistre la fonction en autoload pour qu'elle soit appelée dès qu'on instanciera une classe non déclarée.
