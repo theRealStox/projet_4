661613e6db5d70eb0e790211e3d2b051ee1a7a37
theRealStox <33998824+theRealStox@users.noreply.github.com>
2018-11-22 07:25:35 +0100

    Delete comment__view.php

--- view/backEnd/comment__view.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!isset($_GET['f']) OR isset($_GET['f']) AND $_GET['f'] === 'a') {
-    echo'
-    <section id="comment__listComments" class="standardBlock paddingB2 container allBillets">
-        ' . $comment__showComments . '
-    </section>
-    ';
-}
